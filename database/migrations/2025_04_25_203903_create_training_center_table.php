6d7d135baaf81d1b1940d6b9dd38a699c38df9c6
Stiven <almeidaibarra.stiven@gmail.com>
2025-05-02 15:35:05 -0500

    prueba

--- database/migrations/2025_04_25_203903_create_training_center_table.php
+++ database/migrations/2025_04_25_203903_create_training_center_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        //
         Schema::create('training_center', function (Blueprint $table) {
             $table->id();
             $table->String('name');
